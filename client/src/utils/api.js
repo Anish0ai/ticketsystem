2c21f3d36e02fbe65b66f8c36d135c21b1bdb963
Anish0ai <anishmystical@gmail.com>
2026-05-05 01:52:47 +0530

    Changed backend endpoint

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -5,6 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 const API = axios_1.default.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://ticketsystem-4ygo.vercel.app/",
 });
 exports.default = API;
